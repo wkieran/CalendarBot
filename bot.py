f7afcd80dcefd86d8c7350d4153526958166d49a
wkieran <kieranwhitsitt@outlook.com>
2023-07-28 03:07:28 -0400

    lazy

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 import responses
+import config
 
 
 async def send_message(message, user_message, is_private):
@@ -12,7 +13,6 @@ async def send_message(message, user_message, is_private):
 
 
 def run_discord_bot():
-    TOKEN = 'MTEzNDI5OTU5MDc1ODI0ODUzOA.GmjLIp.K_MD6fN8BPCJKf2mQkMFeJZ1_Ai5GcvIiOexog'
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
